53b857b9cc88a3a3fdea6ee34e962f20e06a5276
Thabo Modise <john.rmodise@gmail.com>
2023-09-09 05:04:37 +0200

    changed posts.jsx

--- api/client/src/Posts/Posts.jsx
+++ api/client/src/Posts/Posts.jsx
@@ -4,9 +4,7 @@ import './Posts.css'
 export default function Posts({posts}) {
   return (
     <div className='posts'>
-     { /*posts.map((p)=>(
-          <Post post ={p}/>
-     ))*/
+     { 
         console.log("posts:", posts)
      }
         
